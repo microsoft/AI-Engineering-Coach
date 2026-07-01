301c25016160dbeafb7e5b7cc873a1df2cb1b338
SecH0us3 <github@ioc.slmail.me>
2026-07-01 08:33:30 +0300

    chore: remove temporary scratch file scratch_db.ts

--- scratch_db.ts
+++
@@ -1,7 +0,0 @@
-import { parseAntigravitySessions } from './src/core/parser-antigravity';
-const sessions = parseAntigravitySessions('/Users/alex/.gemini/antigravity/conversations/');
-for (const s of sessions) {
-  for (const r of s.requests) {
-    console.log(`Req: ${r.requestId}, model: ${r.modelId}, promptTokens: ${r.promptTokens}, completionTokens: ${r.completionTokens}`);
-  }
-}
